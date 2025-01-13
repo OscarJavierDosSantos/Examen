eaf459d3efb66dcaa019bd6927223a024815b2f9
Oscar Javier <oscarjavierdos@gmail.com>
2025-01-13 13:57:22 +0100

    Update css

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ const questionDiv = document.querySelector(".question");
 const resultDiv = document.querySelector(".result");
 const elementP = document.querySelector(".result .selected p");
 let numberSelected = 0;
+
 buttonSend.addEventListener("click", (event) => {
     event.preventDefault();
-    questionDiv.classList.add = "hidden";
-    resultDiv.classList.remove = "hidden";
+    questionDiv.classList.add("hidden");
+    resultDiv.classList.remove("hidden");
     buttons.forEach((button) => {
         if (button.classList.contains("selected")) {
             numberSelected = button.attributes["data-number"].value;
