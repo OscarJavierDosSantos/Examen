6b1db79f08e0610519d48c1b9431bde590bfae29
Oscar Javier <oscarjavierdos@gmail.com>
2025-01-14 12:11:07 +0100

    Change name class

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('.button');
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".button");
     let selectedRating = null;
-  
+
     buttons.forEach((button) => {
-      button.addEventListener('click', function () {
-        buttons.forEach((button) => button.classList.remove('active'));
-        this.classList.add('active');
-        selectedRating = this.getAttribute('data-number');
+        button.addEventListener("click", function () {
+            buttons.forEach((button) => button.classList.remove("active"));
+            this.classList.add("active");
+            selectedRating = this.getAttribute("data-number");
+            if (selectedRating) {
+                sendButton.disabled = false;
+                sendButton.style.backgroundColor = "hsl(25, 97%, 53%)";
+            }
+        });
+    });
+
+    const sendButton = document.querySelector(".send");
+    sendButton.addEventListener("click", function () {
         if (selectedRating) {
-          sendButton.disabled = false;
-          sendButton.style.backgroundColor = 'hsl(25, 97%, 53%)';
+            // Oculta el formulario de comentarios
+            document.querySelector("div.card").classList.add("hidden");
+            // Mostrar la pantalla de confirmación
+            document
+                .querySelector(".confirmationCard")
+                .classList.remove("hidden");
+            // Actualizar la calificación seleccionada
+            document.querySelector(".message .rating").textContent =
+                selectedRating;
+        } else {
+            alert("Por favor, selecciona una valoración antes de enviar.");
         }
-      });
     });
-  
-    const sendButton = document.querySelector('.send');
-    sendButton.addEventListener('click', function () {
-        
-      if (selectedRating) {
-        // Oculta el formulario de comentarios
-        document.querySelector('div.card').classList.add("confirmation");
-        // Mostrar la pantalla de confirmación
-        document.querySelector('.confirmationCard').classList.remove("confirmation");;
-        // Actualizar la calificación seleccionada
-        document.querySelector('.message .rating').textContent = selectedRating;
-      } else {
-        alert('Por favor, selecciona una valoración antes de enviar.');
-      }
-    });
-  });
-  
+});
